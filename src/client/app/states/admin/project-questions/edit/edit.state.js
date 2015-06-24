eea8909599f029d3c049ed42678965019ba9189b
AllenBW <wight_allen@bah.com>
2015-06-24 10:08:25 -0400

    initial commit
    TODO:
    - api work
    - finish connecting form to object

---
+++ src/client/app/states/admin/project-questions/edit/edit.state.js
@@ -0,0 +1,56 @@
+(function() {
+  'use strict';
+
+  angular.module('app.states')
+    .run(appRun);
+
+  /** @ngInject */
+  function appRun(routerHelper, navigationHelper) {
+    routerHelper.configureStates(getStates());
+    navigationHelper.navItems(navItems());
+    navigationHelper.sidebarItems(sidebarItems());
+  }
+
+  function getStates() {
+    return {
+      'admin.project-questions.edit': {
+        url: '/edit/:pqId',
+        templateUrl: 'app/states/admin/project-questions/edit/edit.html',
+        controller: StateController,
+        controllerAs: 'vm',
+        title: 'Edit Project Question',
+        resolve: {
+          projectQuestion: resolveProjectQuestion
+        }
+      }
+    };
+  }
+
+  function navItems() {
+    return {};
+  }
+
+  function sidebarItems() {
+    return {};
+  }
+
+  /** @ngInject */
+  function resolveProjectQuestion(ProjectQuestion, $stateParams){
+    return ProjectQuestion.get({id: $stateParams.pqId}).$promise;
+  }
+
+  /** @ngInject */
+  function StateController(logger, projectQuestion) {
+    var vm = this;
+
+    vm.title = 'Edit Project Question';
+    vm.projectQuestion = projectQuestion;
+    vm.activate = activate;
+
+    activate();
+
+    function activate() {
+      logger.info('Activated Edit Project Question View');
+    }
+  }
+})();
